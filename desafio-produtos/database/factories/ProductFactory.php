6b8a6312708c75e3c1a8ddc5acd71234b96e62cf
gusalberto <gustavoasouzasilva@gmail.com>
2026-05-19 18:26:35 -0300

    Refactor ProductFactory to define model state and initialize product attributes

--- desafio-produtos/database/factories/ProductFactory.php
+++ desafio-produtos/database/factories/ProductFactory.php
@@ -10,6 +10,9 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class ProductFactory extends Factory
 {
+
+    protected $model = Product::class;
+
     /**
      * Define the model's default state.
      *
@@ -18,7 +21,10 @@ class ProductFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->words(2, true),
+            'description' => $this->faker->sentence(),
+            'price' => $this->faker->randomFloat(2, 10, 1000),
+            'stock' => $this->faker->numberBetween(0, 100),
         ];
     }
 }
