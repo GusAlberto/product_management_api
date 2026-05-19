8af8a53302121c7d40aabb5906d42eeb17077001
gusalberto <gustavoasouzasilva@gmail.com>
2026-05-19 18:05:39 -0300

    Update UpdateProductRequest to authorize requests and define validation rules outside of the controller

--- desafio-produtos/app/Http/Requests/UpdateProductRequest.php
+++ desafio-produtos/app/Http/Requests/UpdateProductRequest.php
@@ -12,7 +12,7 @@ class UpdateProductRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -20,10 +20,13 @@ class UpdateProductRequest extends FormRequest
      *
      * @return array<string, ValidationRule|array<mixed>|string>
      */
-    public function rules(): array
+     public function rules(): array
     {
         return [
-            //
+            'name' => ['sometimes', 'required', 'string', 'max:255'],
+            'description' => ['nullable', 'string'],
+            'price' => ['sometimes', 'required', 'numeric', 'min:0'],
+            'stock' => ['sometimes', 'required', 'integer', 'min:0'],
         ];
     }
 }
