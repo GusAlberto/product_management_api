a4af6bb16b22026a52c27c9e2a3e8b41f2d07ea4
gusalberto <gustavoasouzasilva@gmail.com>
2026-05-19 18:27:10 -0300

    Addd product creation test

--- desafio-produtos/tests/Feature/ProductApiTest.php
+++ desafio-produtos/tests/Feature/ProductApiTest.php
@@ -8,13 +8,36 @@ use Tests\TestCase;
 
 class ProductApiTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+    use RefreshDatabase;
+
+    // /**
+    //  * A basic feature test example.
+    //  */
+    // public function test_example(): void
+    // {
+    //     $response = $this->get('/');
+
+    //     $response->assertStatus(200);
+    // }
+
+    public function test_can_create_product(): void
+    {
+        $response = $this->postJson('/api/products', [
+            'name' => 'Mouse Gamer',
+            'description' => 'Mouse RGB',
+            'price' => 199.90,
+            'stock' => 10,
+        ]);
+
+        $response
+            ->assertCreated()
+            ->assertJsonPath('success', true)
+            ->assertJsonPath('data.name', 'Mouse Gamer');
+
+        $this->assertDatabaseHas('products', [
+            'name' => 'Mouse Gamer',
+            'stock' => 10,
+        ]);
     }
 }
