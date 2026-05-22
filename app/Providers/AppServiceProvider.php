c4fc282a4092c263102672e2856077be7ff00965
gusalberto <gustavoasouzasilva@gmail.com>
2026-05-21 22:44:14 -0300

    chore(docs): expose Swagger UI routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Dedoc\Scramble\Scramble;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
@@ -22,6 +23,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Scramble::configure()
+            ->expose(
+                ui: '/docs/api',
+                document: '/docs/api.json',
+            );
+
         RateLimiter::for('products-api', function (Request $request): Limit {
             return Limit::perMinute(5)->by($request->user()?->id ?? $request->ip());
         });
